5ce6f5713e11e19379e682db452f43365c6ef3b0
Fengwei Wang <fewang@microstrategy.com>
2020-05-30 23:19:23 -0400

    add more output

--- selenium/selenium-java/src/main/java/Demo.java
+++ selenium/selenium-java/src/main/java/Demo.java
@@ -6,8 +6,10 @@ public class Demo {
         // Create Driver object for chrome browser
         System.setProperty("webdriver.chrome.driver", "chromedriver");
         WebDriver driver = new ChromeDriver();
-        driver.get("http://google.com");
-        System.out.println(driver.getTitle());
+        driver.get("http://google.com"); // Hit url on the browser
+        System.out.println(driver.getTitle()); // validate if your Page title is correct
+        System.out.println(driver.getCurrentUrl()); // validate if you are landed on correct url
+        System.out.println(driver.getPageSource());
         driver.close();
     }
 }
